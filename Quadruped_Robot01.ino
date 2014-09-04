e5c04bcade4f002073b4f4ffbe2c85bf5796164f
Marcus Krautwurst <tarcus@gmail.com>
2014-09-04 10:11:47 +0200

    Update Quadruped_Robot01.ino

--- Quadruped_Robot01.ino
+++ Quadruped_Robot01.ino
@@ -2,16 +2,17 @@
 
 // All pin definitions
 
-const int PIN_LED_GREEN = 3;
-const int PIN_LED_RED = 4;
+
 const int PIN_BACKSERVO = 9;
 const int PIN_FRONTSERVO = 10;
 const int PIN_ECHO = 11;
 const int PIN_PING = 12;
 const int PIN_STARTBUTTONSTATE = 13;
 
-const int MIN_DISTANCE = 20;           // minimum distance in cm before deciding to walk around obstacles
+
+const int MIN_DISTANCE = 70;              // minimum distance in cm before deciding to walk around obstacles
 const int MAX_DISTANCE = 200;          // maximum distance in cm known to the system
+const int PING_INTERVALL = 1500;
 
 const int ANGLE_NEUTRAL = 80;
 const int FWD_ANGLE1 = 55;
@@ -19,17 +20,22 @@ const int FWD_ANGLE2 = 105;
 const int SIDE_ANGLE1 = 40;
 const int SIDE_ANGLE2 = 120;
 
-const bool DEBUG = true;              // enable some printing
+const bool DEBUG = true;                      // enable some printing
 
 int ISMOVING = false;
-int STEPS_DELAY = 120;                  // delay between steps in ms
+int STEPS_DELAY = 120;                       // delay between steps in ms
 
 int BUTTONSTATE;
 int DISTANCE_CONSTRAINT;
 long DISTANCE;
+long PREVIOUSDISTANCE=0;
+int CURRENT_ANGLE;
 
 
-Servo FRONTSERVO,BACKSERVO;             // our servo objects
+unsigned long time;
+long oldtime = 0;
+
+Servo FRONTSERVO,BACKSERVO;       // our servo objects
 
 
 // Our walk sequences
@@ -43,8 +49,6 @@ void setup()
   pinMode(PIN_STARTBUTTONSTATE,INPUT);
   pinMode(PIN_PING,OUTPUT);
   pinMode(PIN_ECHO, INPUT);
-  pinMode(PIN_LED_RED, OUTPUT);
-  pinMode(PIN_LED_GREEN, OUTPUT);
   FRONTSERVO.attach(PIN_FRONTSERVO);
   BACKSERVO.attach(PIN_BACKSERVO);
 }
@@ -54,73 +58,48 @@ void loop()
   BUTTONSTATE=digitalRead(PIN_STARTBUTTONSTATE);
   if (BUTTONSTATE == HIGH || ISMOVING==true)
   {  
-    ISMOVING = true;
+    ISMOVING = true;    
     
     DISTANCE = getdistance();
-    DISTANCE_CONSTRAINT = constrain(DISTANCE, MAX_DISTANCE, MIN_DISTANCE);
-    STEPS_DELAY  = map(DISTANCE_CONSTRAINT, MAX_DISTANCE,MIN_DISTANCE,400,120); // this is our speed mofifier based on DISTANCE
-      
+    
     if (DISTANCE >= MIN_DISTANCE) 
     {
-      digitalWrite(PIN_LED_GREEN, HIGH);
-      digitalWrite(PIN_LED_RED, LOW);
-      //walk_fwd();
+      walk_fwd();
     } 
     else 
     {
-      digitalWrite(PIN_LED_GREEN, LOW);
-      digitalWrite(PIN_LED_RED, HIGH);
-      //walk_left(); 
+      walk_bck(); 
     }
-  }
+  } 
+  
   else 
   {
     ISMOVING = false;
-  }    
+  }
 } 
 
 long getdistance() 
 {
-  long duration, duration2, cm, cm2;
+  long duration,cm;
   
   // Here we send a ping and measure the time until it bounces back
-//  digitalWrite(PIN_PING, LOW);
-//  delayMicroseconds(2);
-//  digitalWrite(PIN_PING, HIGH);
-//  delayMicroseconds(5);
-//  digitalWrite(PIN_PING, LOW);
-//  
-//  duration = pulseIn(PIN_ECHO, HIGH);
-  pinMode(PIN_PING, OUTPUT);
   digitalWrite(PIN_PING, LOW);
   delayMicroseconds(2);
   digitalWrite(PIN_PING, HIGH);
   delayMicroseconds(5);
   digitalWrite(PIN_PING, LOW);
   
-  pinMode(PIN_ECHO, INPUT);
   duration = pulseIn(PIN_ECHO, HIGH);
   
-  pinMode(PIN_ECHO, OUTPUT);
-  digitalWrite(PIN_ECHO, LOW);
-  delayMicroseconds(2);
-  digitalWrite(PIN_ECHO, HIGH);
-  delayMicroseconds(5);
-  digitalWrite(PIN_ECHO, LOW);
-  
-  pinMode(PIN_PING, INPUT);
-  duration2 = pulseIn(PIN_PING, HIGH);
-  
   cm = microsecondsToCentimeters(duration);
-  cm2 = microsecondsToCentimeters(duration);
+
   if (DEBUG==true)
   {
-    Serial.println();
+    Serial.print("Distance: ");
     Serial.print(cm);
-    Serial.print(" cm and "); 
-    Serial.print(cm2);
-    return cm;
+    Serial.print(" cm\n"); 
   }
+  return cm;
 }
 
 long microsecondsToCentimeters(long microseconds)
